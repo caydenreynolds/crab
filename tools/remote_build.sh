84df8a9161609afd88182c6c4b47e729f458afcb
Cayden Reynolds <cayden@arclasr.net>
2022-07-18 19:35:47 -0400

    Autopush from remote_build.sh

---
+++ tools/remote_build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Usage:
+#  remote_build.sh [options]
+#
+#  Options:
+#  -t --test : Run the test scripts
+#
+#  This script requires that the environment variable CRAB_BUILD_HOST is set
+
+if [[ "$1" == "-t" ]] || [[ "$1" == "--test" ]]; then
+  test="true"
+else
+  test="false"
+fi
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "Script directory: ${script_dir}"
+
+# I'm looking for this literal asterisk character, not trying to glob
+# shellcheck disable=SC2063
+branch="$(git branch | grep "*")"
+branch="${branch:2}"
+echo "On branch $branch"
+
+if [[ "$branch" == "main" ]]; then
+  echo "Don't push to master you dumb bitch"
+  exit 1
+fi
+
+pushd "${script_dir}/../"
+git add .
+git commit -m "Autopush from remote_build.sh"
+git push --set-upstream origin "$branch"
+popd
+
+host_cmd="
+              pushd ~/projects/crab
+              git checkout $branch
+              git pull
+            "
+
+if [[ "$test" == "true" ]]; then
+  echo "Running with robot tests"
+  host_cmd="
+                ${host_cmd}
+                pushd test
+                ./runtests.sh -n
+                tar -ac -f target.tar.gz target/
+                popd
+              "
+else
+  echo "Running without robot tests"
+  host_cmd="
+                ${host_cmd}
+                ./tools/buildall.sh
+              "
+fi
+ssh "$CRAB_BUILD_HOST" "$host_cmd"
+
+if [[ "$test" == "true" ]]; then
+  sftp "${CRAB_BUILD_HOST}:projects/crab/test/target.tar.gz"
+  tar -xf target.tar.gz
+  start target/report.html
+fi
