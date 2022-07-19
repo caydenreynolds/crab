ccd2c988951c1191f51b6d4326dc32e661748897
Cayden Reynolds <cayden@arclasr.net>
2022-07-18 20:13:41 -0400

    Final cleanup

--- tools/dumpast.sh
+++ tools/dumpast.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-mkdir target/
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "Script directory: ${script_dir}"
+
+mkdir "${script_dir}/target/"
 set -e
-../rust/target/debug/print_ast.exe "$@" ../std/ > target/out.ast
+"${script_dir}/../rust/target/debug/print_ast.exe" "$@" ../std/ > "${script_dir}/target/out.ast"
