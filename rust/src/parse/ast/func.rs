ed1361da5d7bafef84585842ca20a4419c1bb407
Cayden Reynolds <cayden@arclasr.net>
2022-07-22 10:29:40 -0400

    Autopush from remote_build.sh

--- rust/src/parse/ast/func.rs
+++ rust/src/parse/ast/func.rs
@@ -82,9 +82,9 @@ impl AstNode for FuncSignature {
         let (pos_params, named_params, return_type) = inner.try_fold((vec![], vec![], CrabType::VOID),
         |(pos_params, named_params, return_type), pair| {
                 Ok(match pair.as_rule() {
-                    rule::pos_params => (PosParams::try_from(pair)?.0, named_params, return_type),
-                    rule::named_params => (pos_params, NamedParams::try_from(pair)?.0, return_type),
-                    rule::return_type => (pos_params, named_params, ReturnType::try_from(pair)?.0)
+                    Rule::pos_params => (PosParams::try_from(pair)?.0, named_params, return_type),
+                    Rule::named_params => (pos_params, NamedParams::try_from(pair)?.0, return_type),
+                    Rule::return_type => (pos_params, named_params, ReturnType::try_from(pair)?.0)
                 })
             }
         )?;
