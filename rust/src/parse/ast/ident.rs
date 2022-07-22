1734e7902e4e3a08be7e820b43d576764df98825
Cayden Reynolds <cayden@arclasr.net>
2022-07-22 18:20:29 -0400

    Autopush from remote_build.sh

--- rust/src/parse/ast/ident.rs
+++ rust/src/parse/ast/ident.rs
@@ -24,7 +24,7 @@ impl AstNode for CrabType {
         where
             Self: Sized,
     {
-        let next = pairs.next().ok_or(ParseError::ExpectedInner)?;
+        let next = pair.into_inner().next().ok_or(ParseError::ExpectedInner)?;
         match next.as_rule() {
             Rule::simple_crab_type => Ok(Self::SIMPLE(SimpleCrabType::try_from(next)?.0)),
             Rule::list_crab_type => Ok(Self::LIST(ListCrabType::try_from(next)?.0)),
@@ -68,7 +68,7 @@ struct SimpleCrabType(Ident);
 try_from_pair!(SimpleCrabType, Rule::simple_crab_type);
 impl AstNode for SimpleCrabType {
     fn from_pair(pair: Pair<Rule>) -> parse::Result<Self> where Self: Sized {
-        Ok(Self(Ident::from(pairs.next().ok_or(ParseError::ExpectedInner)?.as_str())))
+        Ok(Self(Ident::from(pair.into_inner().next().ok_or(ParseError::ExpectedInner)?.as_str())))
     }
 }
 
@@ -76,7 +76,7 @@ struct ListCrabType(Box<CrabType>);
 try_from_pair!(ListCrabType, Rule::list_crab_type);
 impl AstNode for ListCrabType {
     fn from_pair(pair: Pair<Rule>) -> parse::Result<Self> where Self: Sized {
-        Ok(Self(Box::new(CrabType::try_from(pairs.next().ok_or(ParseError::ExpectedInner)?)?)))
+        Ok(Self(Box::new(CrabType::try_from(pair.into_inner().next().ok_or(ParseError::ExpectedInner)?)?)))
     }
 }
 
@@ -85,7 +85,7 @@ try_from_pair!(TmplCrabType, Rule::tmpl_crab_type);
 impl AstNode for TmplCrabType {
     fn from_pair(pair: Pair<Rule>) -> parse::Result<Self> where Self: Sized {
         let mut inner = pair.into_inner();
-        let name = Ident::from(pairs.next().ok_or(ParseError::ExpectedInner)?.as_str());
+        let name = Ident::from(inner.next().ok_or(ParseError::ExpectedInner)?.as_str());
         let tmpls = inner.try_fold(vec![], |tmpls, tmpl| {
             parse::Result::Ok(tmpls.fpush(CrabType::try_from(tmpl)?))
         })?;
